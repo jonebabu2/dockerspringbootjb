c5ac1dd8e1b9dab342952604c82e237fc271542d
Jones Babu <59142450+jonebabu2@users.noreply.github.com>
2020-12-23 14:41:23 +0200

    Update JhooqDockerDemoController.java

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -8,6 +8,6 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Merry Christmas Everyone..!";
+        return "Merry Christmas & Happy New Year Everyone..!";
     }
 }
